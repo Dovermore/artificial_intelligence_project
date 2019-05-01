76b9e30ab33a2439fddfc3ab080365b76e603cbb
chuanyuan <liuchuanyuan@gmail.com>
2019-05-01 22:47:56 +1000

    Modified Json_parser so it can parse any given game stat. I think this would be good for testing agent behaviour Added red_inital_stae.json

--- artificial_idiot/artificial_idiot/util/json_parser.py
+++ artificial_idiot/artificial_idiot/util/json_parser.py
@@ -2,40 +2,39 @@ from collections import defaultdict as dd
 
 
 class JsonParser:
-    block = "block"
+    # map fr_code[i] to to_code[i]
+    fr_code = ["red", "green", "blue"]
+    to_code = ["red", "green", "blue"]
 
-    def __init__(self, json_loader, phase):
-        self.phase = phase
+    def __init__(self, json_loader):
         self.json_loader = json_loader
         self.colour = self.json_loader["colour"]
 
     def parse(self, pos_dict=True):
-        if self.phase == "A":
-            if pos_dict:
-                d = self.parse_pos_a()
-            else:
-                d = self.parse_piece_a()
-            return d, self.colour
+        if pos_dict:
+            d = self.parse_pos_a()
+        else:
+            d = self.parse_piece_a()
+        return d, self.colour
 
     def parse_pos_a(self):
         pos_dict = dd(str)
-        for pos in self.json_loader["pieces"]:
-            pos_dict[tuple(pos)] = self.colour
-        for pos in self.json_loader["blocks"]:
-            pos_dict[tuple(pos)] = self.block
+        for i in range(len(self.fr_code)):
+            for pos in self.json_loader[self.fr_code[i]]:
+                pos_dict[tuple(pos)] = self.to_code[i]
         return pos_dict
 
     def parse_piece_a(self):
         piece_dict = dd(list)
 
-        # Initialise the blocks and pieces no matter what.
+        # Initialise pieces no matter what.
         piece_dict[self.colour] = []
-        piece_dict[self.block] = []
+        for i in range(len(self.fr_code)):
+            piece_dict[self.to_code[i]] = []
 
-        for pos in self.json_loader["pieces"]:
-            piece_dict[self.colour].append(tuple(pos))
-        for pos in self.json_loader["blocks"]:
-            piece_dict[self.block].append(tuple(pos))
+        for i in range(len(self.fr_code)):
+            for pos in self.json_loader[self.fr_code[i]]:
+                piece_dict[self.to_code[i]].append(tuple(pos))
         return piece_dict
 
 
@@ -43,7 +42,18 @@ if __name__ == "__main__":
     # Test for phase
     import json
 
-    with open("tests/part_a/test0.json") as f:
-        json_loader = json.load(f)
-        print(json_loader)
-        json_parser = JsonParser(json_loader, "A")
+
+    def test_inital_state():
+        forward_dict = {(-3, 0): 'red', (-3, 1): 'red', (-3, 2): 'red', (-3, 3): 'red', (0, -3): 'green', (1, -3): 'green', (2, -3): 'green', (3, -3): 'green', (0, 3): 'blue', (1, 2): 'blue', (2, 1): 'blue', (3, 0): 'blue'}
+        backward_dict = {'red': [(-3, 0), (-3, 1), (-3, 2), (-3, 3)], 'green': [(0, -3), (1, -3), (2, -3), (3, -3)], 'blue': [(0, 3), (1, 2), (2, 1), (3, 0)]}
+        with open("../../tests/red_initial_state.json") as f:
+            json_loader = json.load(f)
+            json_parser = JsonParser(json_loader)
+            print(json_parser.parse_piece_a())
+            print("ans: ", str(backward_dict))
+            print(json_parser.parse_pos_a())
+            print("ans: ", str(forward_dict))
+
+
+    # test
+    test_inital_state()
