c87cd69c88b939f26530d847872f0bb3518f1d2d
Dovermore <calvinyeye0627@gmail.com>
2019-05-13 15:07:30 +1000

    better concat and some documentation for machine learning module, better structure

--- artificial_idiot/artificial_idiot/machine_learning/layer.py
+++ artificial_idiot/artificial_idiot/machine_learning/layer.py
@@ -4,25 +4,68 @@ from abc import abstractmethod, ABC
 class AbstractLayer(ABC):
 
     @abstractmethod
-    def forward(self, inputs):
+    def forward(self, inputs, training=True):
+        """
+        Compute forward value of a layer
+        :param inputs: The input to the layer, dimension must agree
+        :param training: specifies how the behave differently for different
+            phase
+        :return: Forward layer output
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def train_forward(self, inputs):
+    def get_dz(self, z, backward_err):
+        """
+        Compute error of a layer using the unactivated value z and error from
+        output layer.
+        :param z: The value of
+        :param backward_err:
+        :return:
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def get_layer_error(self, z, backwarded_err):
+    def get_da_prev(self, z):
+        """
+        Compute derivative for activation function of previous layer
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def backward(self, layer_err):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def get_grad(self, inputs, layer_err):
+    def get_dw(self, inputs, layer_err):
         raise NotImplementedError()
 
     @abstractmethod
     def update(self, grad):
         raise NotImplementedError()
+
+
+class FullyConnected(AbstractLayer):
+    def __init__(self, wshape, activation, weight_init):
+        self.wshape = wshape
+        self.W = weight_init(self.wshape)
+        self.activation = activation
+
+    def forward(self, inputs, training=False):
+        z = inputs.dot(self.W)
+        a = self.activation.compute(z)
+        if training:
+            return z, a
+        return a
+
+    def get_dz(self, z, backwarded_err):
+        return backwarded_err * self.activation.deriv(z)
+
+    def get_da_prev(self, z):
+        """
+        Chain rule, z = X @ w, a = relu(z)
+        dy/dw = dy/dz * dz/dw
+        """
+        return z.dot(self.W.T)
+
+    def get_dw(self, inputs, layer_err):
+        return inputs.T.dot(layer_err)
+
+    def update(self, grad):
+        self.W -= grad
--- artificial_idiot/artificial_idiot/machine_learning/network.py
+++ artificial_idiot/artificial_idiot/machine_learning/network.py
@@ -1,46 +1,86 @@
 from collections import deque
+import pickle # Python serialisation library
 
 
-class Network(object):
+class Network:
+    """
+    This class defines how to train the neural network
+    """
     def __init__(self, layers, learning_rate, loss):
+        """
+        __init__ the nerwork class for training, by specifying a set of layers
+        the dimension of layers must agree.
+        A learning rate, and a loss function to compute lsos with
+        :param layers: :AbstractBaseLayer: Layers that the data will pass
+            through
+        :param learning_rate: The learning rate of the network
+        :param loss: Loss function to use.
+        """
         self.layers = layers
         self.loss = loss
         self._learning_rate = learning_rate
 
     @property
     def learning_rate(self):
+        """
+        Gett of the learning rate of the network
+        :return: The learning rate
+        """
         return self._learning_rate
 
     @learning_rate.setter
-    def learning_rate(self, v):
-        self._learning_rate = v
+    def learning_rate(self, theta):
+        """
+        Setter of the learning rate
+        :param theta: The value to set the learning rate to.
+        """
+        self._learning_rate = theta
 
     def forward(self, inputs):
+        """
+        Forward propagation of the neural network (or linear model)
+        :param inputs: The input vector
+        :return: The output of network
+        """
         activation = inputs
         for l in self.layers:
             activation = l.forward(activation)
         return activation
 
-    def train_step(self, mini_batch):
+    def backward(self, mini_batch):
+        """
+        One pass of backward propagation, updating parameters based on the
+        given mini_batch of data
+        :param mini_batch: The minibatch of data to train on
+        """
+
+        # X -> z1 -> a1 -> z2 -> a2 ... -> zn -> y_hat, loss = J(y - y_hat)
         mini_batch_inputs, mini_batch_outputs = mini_batch
         zs = deque([mini_batch_inputs])
         activation = mini_batch_inputs
-        for l in self.layers:
-            z, activation = l.train_forward(activation)
+        for layer in self.layers:
+            z, activation = layer.forward(activation, True)
             zs.appendleft(z)
+        d_yhat = self.loss.deriv((activation, mini_batch_outputs))
+        z = zs.popleft()
+        da = d_yhat
+        gradients = deque()
+        for layer in reversed(self.layers):
+            # DJ/Dz_i = DJ/Da_i * Da_i/Dz_i
+            dz = layer.get_dz(z, da) #local
+            z = zs.popleft()
+            # DJ/Dw_i = DJ/Dz_i * Dz_i/Dw_i
+            gradient = layer.get_dw(z, dz)
+            gradients.appendleft(gradient)
+            # DJ/Da_i-1 = DJ/Dz_i * Dz/Da_i-1
+            da = layer.get_da_prev(dz)
 
-        loss_err = self.loss.deriv((activation, mini_batch_outputs))
-        lz = zs.popleft()
-        backwarded_err = loss_err
-        grads = deque()
-        for l in reversed(self.layers):
-            layer_err = l.get_layer_error(lz, backwarded_err) #local
-            lz = zs.popleft()
-            grads.appendleft(l.get_grad(lz, layer_err))
-            backwarded_err = l.backward(layer_err) # backwarded error
+        for layer in self.layers:
+            # W_t+1 = W_t - theta * gard
+            layer.update(self.learning_rate * gradients.popleft())
 
-        # update step
-        for l in self.layers:
-            l.update(self.learning_rate * grads.popleft())
+        assert len(gradients) == 0
 
-        assert len(grads) == 0
+    def serialise(self):
+        # pickle.
+        pass
