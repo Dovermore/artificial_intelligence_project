e505448c30679e3c4b552fc781c20c0251924cad
Dovermore <calvinyeye0627@gmail.com>
2019-05-18 17:06:42 +1000

    change some minimax

--- artificial_idiot/artificial_idiot/search/mini_max.py
+++ artificial_idiot/artificial_idiot/search/mini_max.py
@@ -16,18 +16,19 @@ class AlphaBetaSearch(Search):
             return utility('red'), None
         depth += 1
         v = +inf
+        action = None
         # take two steps assuming opponents are in alliance
         for green_action in game.actions(state):
             state_1 = game.result(state, green_action)
             for blue_action in game.actions(state_1):
                 state_2 = game.result(state_1, blue_action)
-                v_, _ = self.max_value(game, state_2, depth, a, p)
+                v_, action = self.max_value(game, state_2, depth, a, p)
                 # print(f'{depth} {green_action} {blue_action} {float(v_):.2}')
                 v = min(v, v_)
                 if v <= a:
-                    return v, _
+                    return v, action
                 p = min(p, v)
-        return v, _
+        return v, action
 
     def max_value(self, game, state, depth, a, p):
         if self.terminal_test(state, depth):
@@ -35,7 +36,7 @@ class AlphaBetaSearch(Search):
             return utility('red'), None
         depth += 1
         v = -inf
-        best_action = None
+        action = None
         for action in game.actions(state):
             # print(action)
             s_ = game.result(state, action)
@@ -43,13 +44,13 @@ class AlphaBetaSearch(Search):
             # print(f'{depth} {action} {float(v_):.2}')
             # print(a, p)
             if v_ > v:
-                best_action = action
+                action = action
                 v = v_
             # opponent won't allow you to chose a better move
             if v >= p:
                 return v, action
             a = max(a, v)
-        return v, best_action
+        return v, action
 
     def search(self, game, state, depth=0):
         best_v, best_action = self.max_value(game, state, depth, -inf, inf)
