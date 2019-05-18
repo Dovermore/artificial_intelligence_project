166722fbbcfa3ec1286c0c3742f0e0fad56724c5
Dovermore <calvinyeye0627@gmail.com>
2019-05-18 10:08:24 +1000

    some more transport from part A to part B

--- artificial_idiot/artificial_idiot/search/a_star.py
+++ artificial_idiot/artificial_idiot/search/a_star.py
@@ -1,6 +1,9 @@
 from artificial_idiot.search.search import Search
 from artificial_idiot.util.queue import PriorityQueueImproved
 from artificial_idiot.game.node import Node
+from artificial_idiot.game.state import State
+from artificial_idiot.game.game import Problem, Game
+from collections import defaultdict as dd
 
 
 class AStar(Search):
@@ -9,12 +12,20 @@ class AStar(Search):
     when the player is not near enemy and have enough piece to exit to win.
     (Note this includes case where only one player is left.)
     """
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
         self.solution = None
+        # remove all agent's pieces
+        self.goal = None
+
+        # A mapping for heuristic distances
+        self.colour = None
+        self.heuristic_distance = dd(float)
 
     def search(self, game, state, **kwargs):
         if self.solution is None:
+            # TODO finish this part
+            self._build_heuristic_distance()
             self.astar_search(game, )
 
     @classmethod
@@ -57,6 +68,44 @@ class AStar(Search):
         else in your Problem subclass."""
         h = h or problem.h
         # h = memoize(h or problem.h, 'h')
-        return cls.best_first_graph_search(problem, lambda n: n.path_cost + h(n),
-                                       *args, **kwargs)
+        return cls.best_first_graph_search\
+            (problem, lambda n: n.path_cost + h(n), *args, **kwargs)
 
+    def _build_heuristic_distance(self, problem):
+        """
+        Build the heuristic map used for searching by Dijkstra's Algorithm
+        """
+        goal = self.goal
+        frontier = PriorityQueueImproved('min',
+                                         f=self.heuristic_distance.__getitem__)
+        # For all exit positions
+        for pos in problem._exit_positions[self.colour]:
+            # If the exit position is not occupied by other pieces
+            if pos not in goal.pos_to_piece:
+                # Set initial heuristic to 1, and add to start
+                self.heuristic_distance[pos] = 1
+                frontier.append(pos)
+
+        # While search is not ended
+        while frontier:
+            pos = frontier.pop()
+            q, r = pos
+            # Explore all space near current place
+            cost = self.heuristic_distance[pos]
+            for dq, dr in problem._move:
+                for move in range(1, 3):
+                    next_pos = (q + dq * move, r + dr * move)
+                    # If the moved position is valid, update it with cost + 1,
+                    # Else simply continue next loop
+                    if (not State.inboard(next_pos) or
+                            next_pos in goal.pos_to_piece):
+                        continue
+                    # Get value in dictionary
+                    h_val = self.heuristic_distance.get(next_pos, None)
+
+                    # Not yet navigated to or can be updated
+                    if h_val is None or h_val > cost + 1:
+                        # Update dictionary entry
+                        self.heuristic_distance[next_pos] = cost + 1
+                        # Update the value in queue
+                        frontier.append(next_pos)
