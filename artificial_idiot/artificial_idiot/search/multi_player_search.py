4d0352a9b18f075aa7490178016df26757597c6c
Dovermore <calvinyeye0627@gmail.com>
2019-05-20 11:34:57 +1000

    basic search behavior

--- artificial_idiot/artificial_idiot/search/multi_player_search.py
+++ artificial_idiot/artificial_idiot/search/multi_player_search.py
@@ -2,13 +2,14 @@ from artificial_idiot.search.search import Search
 
 
 class MultiPlayerSearch(Search):
-    #  3 player -> beginning: book + search + a star
-    def __init__(self, book=None, search=None, a_star=None):
+    #  3 player -> beginning: book + search
+    def __init__(self, book=None, search=None):
         self.book = book
         self.book_finished = False
         self.search = search
-        self.a_star = a_star
 
     def search(self, game, state, **kwargs):
-        pass
+        if not self.book_finished:
+            return self.book.search(game, state, **kwargs)
+        return self.search.search(game, state, **kwargs)
 
