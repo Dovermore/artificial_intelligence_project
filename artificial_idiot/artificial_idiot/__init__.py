47444a28a9c509090243a379e97fba85ad8b1cd3
Dovermore <calvinyeye0627@gmail.com>
2019-05-20 17:29:02 +1000

    more improvement

--- artificial_idiot/artificial_idiot/__init__.py
+++ artificial_idiot/artificial_idiot/__init__.py
@@ -4,10 +4,24 @@ print("======================")
 pp = ParanoidPlayer_Naive
 mn = MaxNPlayer
 
-weights = [10, -2, 4, -0.5, 2, 0.5]
+"""
+* weights are defined beforehand
+An evaluator that considers
+1. (max) number of player piece
+2. (max neg) leading player's distance from winning
+3. (max) distance of excess piece to opponent exit,
+    try to block leading player from exiting
+4. (max neg) networth of other players' pieces
+5. (max) negative sum distance to goal
+6. (max) number of completed piece
+7. (max) number of excess piece
+"""
+
+weights = [100, -10, 6, -20, 5, 20, 80]
 evaluator_generator = MinimaxEvaluator(weights)
 cutoff = DepthLimitCutoff(3)
-open_book = OpenGameBook("gather")
+# open_book = OpenGameBook("gather")
+open_book = None
 search = AlphaBetaSearch(evaluator_generator, cutoff)
 
 three_player = MultiPlayerSearch(book=open_book, search_algorithm=search)
